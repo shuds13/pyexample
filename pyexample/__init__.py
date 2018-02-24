49dec8171172c1049b765e45bc2e746cbda9b625
shudson <shudson@anl.gov>
2018-02-23 23:14:16 -0600

    Uncomment lines in __init__.py

--- pyexample/__init__.py
+++ pyexample/__init__.py
@@ -4,6 +4,6 @@ pyexample.
 An example python library.
 """
 
-#__version__ = "0.1.0"
-#__author__ = 'Stephen Hudson'
+__version__ = "0.1.0"
+__author__ = 'Stephen Hudson'
 __credits__ = 'Argonne National Laboratory'
